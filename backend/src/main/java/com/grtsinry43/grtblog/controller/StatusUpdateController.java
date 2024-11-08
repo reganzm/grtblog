93e6225fb14491c25868fe0e72de4488d490283a
grtsinry43 <grtsinry43@outlook.com>
2024-11-08 18:05:10 +0800

    💬style(backend): 准备重建后端的存储结构

--- backend/src/main/java/com/grtsinry43/grtblog/controller/StatusUpdateController.java
+++ backend/src/main/java/com/grtsinry43/grtblog/controller/StatusUpdateController.java
@@ -4,6 +4,7 @@ import com.grtsinry43.grtblog.dto.ApiResponse;
 import com.grtsinry43.grtblog.service.impl.StatusUpdateServiceImpl;
 import com.grtsinry43.grtblog.vo.StatusUpdatePreview;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,21 @@ public class StatusUpdateController {
     public ApiResponse<List<StatusUpdatePreview>> getLastFourStatusUpdates() {
         List<StatusUpdatePreview> lastFourStatusUpdates = statusUpdateService.getLastFourStatusUpdates();
         return ApiResponse.success(lastFourStatusUpdates);
+    }
+
+    @GetMapping("/ids")
+    public ApiResponse<List<String>> getAllStatusUpdateIds() {
+        List<Long> allStatusUpdateIds = statusUpdateService.getAllStatusUpdateIds();
+        // 转成字符串防止精度丢失
+        List<String> allStatusUpdateIdsStr = allStatusUpdateIds.stream().map(String::valueOf).toList();
+        return ApiResponse.success(allStatusUpdateIdsStr);
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<StatusUpdatePreview> viewOneStatusUpdateApi(@PathVariable String id) {
+        Long idLong = Long.parseLong(id);
+        StatusUpdatePreview statusUpdatePreview = statusUpdateService.viewOneStatusUpdate(idLong);
+        return ApiResponse.success(statusUpdatePreview);
     }
 
 }
