f94546a867a92c250c69eb4af9fbacf1965e0376
grtsinry43 <grtsinry43@outlook.com>
2025-02-22 19:14:29 +0800

    🐛fix(backend): 错误的 deepseek 调用修正

--- backend/src/main/java/com/grtsinry43/grtblog/service/AIClient.java
+++ backend/src/main/java/com/grtsinry43/grtblog/service/AIClient.java
@@ -24,8 +24,8 @@ import java.util.Map;
 
 @Component
 public class AIClient {
-//    private static final String API_URL = "https://api.deepseek.com/chat/completions";
-    private static final String API_URL = "https://ark.cn-beijing.volces.com/api/v3/chat/completions";
+    private static final String API_URL = "https://api.deepseek.com/chat/completions";
+//    private static final String API_URL = "https://ark.cn-beijing.volces.com/api/v3/chat/completions";
 
     @Value("${com.grtsinry43.deepseek-api-key}")
     private String apiKey;
@@ -41,8 +41,8 @@ public class AIClient {
     public String generateSummary(String taskId, String content, String model) throws IOException {
         // 构造 JSON 请求体
         Map<String, Object> requestBody = new HashMap<>();
-//        requestBody.put("model", model);
-        requestBody.put("model", "ep-20250127215805-dbdr8");
+        requestBody.put("model", model);
+//        requestBody.put("model", "ep-20250127215805-dbdr8");
         requestBody.put("messages", List.of(
                 Map.of("role", "system", "content", "你是DeepSeek-R1，作为先进的语言模型，在接下来的所有聊天中，我将会给你一篇博客文章（md格式），你需要分析这个博文的内容，给出总结，总结内容尽量在200字左右，你可以使用加粗斜体链接代码块等markdown语法，条例清晰，总结概括完整，有先导和总结的意义，要求开头用诗意和深刻的语言，写几句骈句作为开头，下面的内容若文章技术类则总结偏向逻辑，如果偏向生活类则总结偏向文学唯美，注意要 pangu 混排格式化，保证观感舒适"),
                 Map.of("role", "user", "content", content)
